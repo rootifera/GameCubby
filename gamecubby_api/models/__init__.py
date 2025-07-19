3ce0cf701f01a4b3f6d9a0f625d0d135668db989
Omur Ozbahceliler <omur@rootifera.org>
2025-07-19 19:43:38 +0100

    alembic import fix

--- gamecubby_api/models/__init__.py
+++ gamecubby_api/models/__init__.py
@@ -1,3 +1,12 @@
 from sqlalchemy.orm import declarative_base
 
 Base = declarative_base()
+
+from .collection import Collection
+from .game_platform import game_platforms
+from .game import Game
+from .game_tag import game_tags
+from .location import Location
+from .platform import Platform
+from .storage import GameFile
+from .tag import Tag
