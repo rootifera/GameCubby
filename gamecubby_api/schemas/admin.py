c3b9f0d096755b6061d74c1b583b4310629b77dd
Omur Ozbahceliler <omur@rootifera.org>
2026-05-29 10:52:59 +0100

    code clean up midway

--- gamecubby_api/schemas/admin.py
+++ gamecubby_api/schemas/admin.py
@@ -4,8 +4,3 @@ from pydantic import BaseModel
 class LoginRequest(BaseModel):
     username: str
     password: str
-
-
-class PasswordChangeRequest(BaseModel):
-    current_password: str
-    new_password: str
